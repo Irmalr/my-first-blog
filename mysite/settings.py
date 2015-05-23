b5d5cbdf9ce4d2aa1eed2f3334aa591bc270c65b
Irma <irmapeqenaa@gmail.com>
2015-05-23 16:34:58 -0700

    to

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,7 +101,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = True
