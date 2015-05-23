71d19e3dbda1181ffc175426297f1789c0a65270
Irma <irmapeqenaa@gmail.com>
2015-05-23 15:46:14 -0700

    fixed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,13 +101,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+STATIC_ROOT = 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = 'staticfiles'
-DEBUG = False
-
-from .local_settings import *
-except ImportError:
-    pass
+DEBUG = True
